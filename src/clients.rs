49676e4babd9f27d9f9a63ea0907f69386b4c8ab
sidney <sidneymillen1@gmail.com>
2025-11-12 16:00:18 -0500

    another benchmark

--- src/clients.rs
+++ src/clients.rs
@@ -8,12 +8,47 @@ pub struct StaticKeyClient {
 
 impl StaticKeyClient {
     pub fn new() -> Self {
-        let csprng = OsRng;
-        let secret = StaticSecret::random_from_rng(csprng);
+        let secret = StaticSecret::random_from_rng(OsRng);
         let pubkey = PublicKey::from(&secret);
-        StaticKeyClient { secret, pubkey }
+        Self { secret, pubkey }
     }
     pub fn calculate_shared_secret(&self, other_pubkey: &PublicKey) -> SharedSecret {
         self.secret.diffie_hellman(other_pubkey)
     }
+}
+
+#[derive(Clone, Copy)]
+pub struct ClientID(u8);
+
+pub struct TmpKeyClient {
+    shared_secrets: Vec<(SharedSecret, ClientID)>,
+    pub id: ClientID,
+}
+
+impl TmpKeyClient {
+    pub fn new(id: u8) -> Self {
+        Self {
+            shared_secrets: vec![],
+            id: ClientID(id),
+        }
+    }
+    pub fn add_shared_secret(
+        &mut self,
+        s: EphemeralSecret,
+        other_pubkey: &PublicKey,
+        other_id: ClientID,
+    ) {
+        self.shared_secrets
+            .push((s.diffie_hellman(other_pubkey), other_id));
+    }
+}
+pub fn tmp_client_key_exchange(client: &mut TmpKeyClient, other_client: &mut TmpKeyClient) {
+    let secret1 = EphemeralSecret::random_from_rng(OsRng);
+    let pubkey1 = PublicKey::from(&secret1);
+
+    let secret2 = EphemeralSecret::random_from_rng(OsRng);
+    let pubkey2 = PublicKey::from(&secret2);
+
+    client.add_shared_secret(secret1, &pubkey2, other_client.id);
+    other_client.add_shared_secret(secret2, &pubkey1, client.id);
 }
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use itertools::Itertools;
     use test::Bencher;
 
     #[bench]
@@ -35,6 +36,28 @@ mod tests {
                     }
                     //generate a shared secret for each pair of clients. each client needs to compute the secret from the other client's pubkey
                     client.calculate_shared_secret(&other_client.pubkey);
+                }
+            }
+        });
+    }
+    #[bench]
+    fn ten_party_ephemeral_key_benchmark(b: &mut test::Bencher) {
+        let mut clients: Vec<TmpKeyClient> = vec![];
+
+        for i in 1..10 {
+            clients.push(TmpKeyClient::new(i))
+        }
+
+        b.iter(|| {
+            //we have to iter over all pairs of clients since each for each exchange we need to generate a new ephemeral key on both clients
+            for i in 0..clients.len() {
+                for j in (i + 1)..clients.len() {
+                    // Split the slice to get two mutable references
+                    let (left, right) = clients.split_at_mut(j);
+                    let client = &mut left[i];
+                    let other_client = &mut right[0];
+
+                    tmp_client_key_exchange(client, other_client);
                 }
             }
         });
