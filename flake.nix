16c9a0d529bfc7157db39c284f80bbb8d3eddca4
sidney <sidneymillen1@gmail.com>
2025-11-21 14:11:57 -0500

    new branch. redid dev environment to use a normal nix flake. added basic macroquad stuff

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description =
+    "An ad-hoc macroquad development flake, forked from my bevy development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+
+            libGL
+
+            # X11 dependencies
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+
+            # Wayland dependencies
+            libxkbcommon
+            wayland
+
+            # Development tools
+            cargo-watch
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${
+              with pkgs;
+              pkgs.lib.makeLibraryPath [
+                xorg.libX11
+                libxkbcommon
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr
+                libGL
+              ]
+            }:$LD_LIBRARY_PATH
+          '';
+        };
+      });
+}
